Working Jacobian model, incorrect movement.

model/kinematics/model implementation/kinematics_validation_model.py
@@ -18,15 +18,227 @@ Output:
 - Error due to changes in link longitude.
 
 """
-
 import numpy as np
 import tkinter as tk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+class Vector:
+    def __init__(self, values):
+        self.v = values
+
+    def __len__(self):
+        return len(self.v)
+
+    def __getitem__(self, index):
+        return self.v[index]
+
+    def __add__(self, other):
+        r = []
+
+        for i in range(len(self)):
+            r.append(self[i] + other[i])
+
+        return Vector(r)
+
+    def __sub__(self, other):
+        r = []
+
+        for i in range(len(self)):
+            r.append(self[i] - other[i])
+
+        return Vector(r)
+
+    def __mul__(self, scalar):
+        r = []
+
+        for i in range(len(self)):
+            r.append(self[i] * scalar)
+
+        return Vector(r)
+
+    def dot(self, other):
+        d = 0
+
+        for i in range(len(self)):
+            d += self[i] * other[i]
+
+        return d
+
+    def __repr__(self):
+        return f"Vector({self.v})"
+    
+
+class Matrix:
+    def __init__(self, rows):
+        self.m = rows
+
+
+    def shape(self):
+        return (len(self.m), len(self.m[0]))
+
+
+    def __getitem__(self, index):
+        return self.m[index]
+
+
+    def __matmul__(self, other):
+        rows_A = len(self.m)
+        cols_A = len(self.m[0])
+
+        rows_B = len(other.m)
+        cols_B = len(other.m[0])
+
+        if cols_A != rows_B:
+            raise Exception("Invalid dimensions")
+
+        C = []
+
+        for i in range(rows_A):
+            row = []
+
+            for j in range(cols_B):
+
+                d = 0
+
+                for k in range(cols_A):
+                    d += self.m[i][k] * other.m[k][j]
+
+                row.append(d)
+
+            C.append(row)
+
+        return Matrix(C)
+
+
+    def apply(self, v):
+
+        r = []
+
+        for i in range(len(self.m)):
+
+            d = 0
+
+            for k in range(len(self.m[0])):
+                d += self.m[i][k] * v[k]
+
+            r.append(d)
+
+        return Vector(r)
+    
+
+    @staticmethod
+    def identity(e):
+        B = []
+        for i in range(e):
+            row = []
+            for j in range(e):
+                if(i==j):
+                    row.append(1)
+                else:
+                    row.append(0)
+            B.append(row)
+
+        return(Matrix(B))
+    
+
+    def gauss_exchange(self,i,j):
+        for k in range(len(self.m[0])):
+            t=self.m[i][k]
+            self.m[i][k]=self.m[j][k]
+            self.m[j][k]=t
+
+    def gauss_sum(self,i,j,e):
+        for k in range(len(self.m[0])):
+            self.m[i][k]=self.m[i][k]+e*self.m[j][k]
+
+    def gauss_mult(self,i,j):
+        for k in range(len(self.m[0])):
+            self.m[i][k]=self.m[i][k]*j
+
+    def copy(self):
+        B = []
+        for i in range(len(self.m)):
+            row = []
+            for j in range(len(self.m[0])):
+                row.append(self.m[i][j])
+            B.append(row)
+
+        return Matrix(B)
+
+
+    def inverse(self):
+
+        A = self.copy()
+
+        rows_A = len(A.m)
+        cols_A = len(A.m[0])
+
+        if rows_A != cols_A:
+            raise Exception("Invalid dimensions")
+
+        B = Matrix.identity(rows_A)
+
+        eps = 1e-12
+
+        for i in range(cols_A):
+
+            # Searching for a non 0 number in column i
+            pivot_row = i
+
+            while pivot_row < rows_A and abs(A.m[pivot_row][i]) < eps:
+                pivot_row += 1
+
+            # If all the column is 0
+            if pivot_row == rows_A:
+                raise Exception("Singular matrix")
+
+            # Exchange rows to locate pivot in matrix diagonal
+            if pivot_row != i:
+                A.gauss_exchange(i, pivot_row)
+                B.gauss_exchange(i, pivot_row)
+
+            # Turn pivot into 1
+            pivot = A.m[i][i]
+
+            A.gauss_mult(i, 1/pivot)
+            B.gauss_mult(i, 1/pivot)
+
+            # Turn all numbers under diagonal to 0
+            for j in range(i+1, rows_A):
+
+                if abs(A.m[j][i]) > eps:
+
+                    factor = -A.m[j][i]
+
+                    A.gauss_sum(j, i, factor)
+                    B.gauss_sum(j, i, factor)
+
+        # Using found pivots, turn numbers above diagonal into 0
+
+        for i in range(cols_A-1, -1, -1):
+
+            for j in range(i-1, -1, -1):
+
+                if abs(A.m[j][i]) > eps:
+
+                    factor = -A.m[j][i]
+
+                    A.gauss_sum(j, i, factor)
+                    B.gauss_sum(j, i, factor)
+
+        return B
+
+
+    def __repr__(self):
+        return f"Matrix({self.m})"
+
+
+
+
 
 def rot_x(theta):
-    return np.array([
+    return Matrix([
         [1, 0, 0, 0 ],
         [0, np.cos(theta), -np.sin(theta), 0 ],
         [0, np.sin(theta), np.cos(theta), 0 ],
@@ -34,7 +246,7 @@ def rot_x(theta):
     ])
 
 def rot_y(theta):
-    return np.array([
+    return Matrix([
         [np.cos(theta), 0, np.sin(theta), 0 ],
         [0, 1, 0, 0 ],
         [-np.sin(theta), 0, np.cos(theta), 0 ],
@@ -42,7 +254,7 @@ def rot_y(theta):
     ])
 
 def rot_z(theta):
-    return np.array([
+    return Matrix([
         [np.cos(theta), -np.sin(theta), 0, 0 ],
         [np.sin(theta), np.cos(theta), 0, 0 ],
         [0, 0, 1, 0 ],
@@ -50,85 +262,68 @@ def rot_z(theta):
     ])
 
 def movement(x, y, z):
-    return np.array([
+    return Matrix([
         [1, 0, 0, x ],
         [0, 1, 0, y ],
         [0, 0, 1, z ],
         [0, 0, 0, 1]
     ])
 
-def direct(pf, j, zero):
-    A0=rot_z(j[0]) @ movement(0, 0, l0)
-    A1=rot_y(j[1]) @ movement(0, 0, l1)
-    A2=rot_y(j[2]) @ movement(0, 0, l2)
+def direct_k(j, zero, l):
+    A0=rot_z(j[0]) @ movement(0, 0, l[0])
+    A1=rot_y(j[1]) @ movement(0, 0, l[1])
+    A2=rot_y(j[2]) @ movement(0, 0, l[2])
 
-    pf[3]=A0@A1@A2@zero
+    p3=Matrix.apply(A0@A1@A2,zero)
 
-    pf[2]=A0@A1@zero
+    p2=Matrix.apply(A0@A1,zero)
 
-    pf[1]=A0@zero
+    p1=Matrix.apply(A0,zero)
 
-def inverse(pr, j):
-    j[0] = np.atan2(pr[1],pr[0])
+    p0=zero
+
+    return([p0,p1,p2,p3])
+
+def inverse_k(pr, l):
+    
+    j=[]
+    j.append(np.atan2(pr[1],pr[0])) 
 
     ## Limitation on arccos input between -1 and 1
-    D = (pr[0]**2 + pr[1]**2 + (pr[2]-l0)**2 - l1**2 - l2**2)/(2*l1*l2)
+    D = (pr[0]**2 + pr[1]**2 + (pr[2]-l[0])**2 - l[1]**2 - l[2]**2)/(2*l[1]*l[2])
     D = np.clip(D, -1.0, 1.0)
 
-    j[2] = np.arccos(D) 
+    t = np.arccos(D) 
 
-    j[1] = np.atan2((pr[0]**2+pr[1]**2)**(0.5), pr[2]-l0) - np.atan2(l2*np.sin(j[2]), l1+l2*np.cos(j[2]))
+    j.append(np.atan2((pr[0]**2+pr[1]**2)**(0.5), pr[2]-l[0]) - np.atan2(l[2]*np.sin(t), l[1]+l[2]*np.cos(t)))
+    j.append(t)
+    return Vector(j)
+    
+
+
 
 ##Joint angles
 
-j0 = 0
-j1 = 0
-j2 = np.pi/2
-
-j = [j0, j1, j2] 
+j = Vector([0, 0, np.pi/2])
 
 
 ##Link lenght
 
-l0 = 1
-l1 = 2
-l2 = 2
-
-l = [l0, l1, l2] 
+l = ([1, 2, 2]) 
 
 
 ##Each joint position in the world frame
 
-zero = np.array([
-    [0],
-    [0],
-    [0],
-    [1]
-])
+zero = Vector([0,0,0,1])
 
 
-p0 = np.array([
-    [0],
-    [0],
-    [l0],
-    [1]
-])
+p0 = Vector([0,0,l[0],1])
 
-p1 = np.array([
-    [0],
-    [0],
-    [l0+l1],
-    [1]
-])
+p1 = Vector([0,0,l[0]+l[1],1])
 
-p2 = np.array([
-    [0],
-    [0],
-    [l0+l1+l2],
-    [1]
-])
+p2 = Vector([0,0,l[0]+l[1]+l[2],1])
 
-pf = [zero, p0, p1, p2]
+pf = Matrix([zero, p0, p1, p2])
 
 
 
@@ -152,8 +347,8 @@ def vector_draw():
     pr = [x, y, z]
 
     # Based on desired end effector position, calculate each joint angle, stored in j
-    inverse(pr, j)
-    direct(pf, j, zero)
+    j=inverse_k(pr, l)
+    pf=direct_k(j, zero, l)
 
     # Clear global space
     ax.clear()
@@ -163,12 +358,19 @@ def vector_draw():
 
     # Draw each link, represented by a vector
     for i in range(len(pf)-1):
-        ax.quiver(pf[i][0], pf[i][1], pf[i][2], pf[i+1][0]-pf[i][0], pf[i+1][1]-pf[i][1], pf[i+1][2]-pf[i][2])
-        link_long.append(((pf[i+1][0]-pf[i][0])**2 + (pf[i+1][1]-pf[i][1])**2 + (pf[i+1][2]-pf[i][2])**2)**0.5)
 
-    # Draw each joint, represented by a point
-    for p in pf:
-        ax.scatter(p[0,0], p[1,0], p[2,0])
+        p0 = pf[i][:3]
+        p1 = pf[i+1][:3]
+
+        ax.quiver(
+            p0[0], p0[1], p0[2],
+            p1[0]-p0[0],
+            p1[1]-p0[1],
+            p1[2]-p0[2]
+        )
+        
+        link_long.append(((p1[0]-p0[0])**2 + (p1[1]-p0[1])**2 + (p1[2]-p0[2])**2)**0.5)
+
     
     # Write each result variable
     angle_text.set(
@@ -178,9 +380,9 @@ def vector_draw():
     )
 
     link_long_error.set(
-    f"l0={abs(l0-link_long[0])}  "
-    f"l1={abs(l1-link_long[1])}  "
-    f"l2={abs(l2-link_long[2])}"
+    f"l0={abs(l[0]-link_long[0])}  "
+    f"l1={abs(l[1]-link_long[1])}  "
+    f"l2={abs(l[2]-link_long[2])}"
     )
 
     position_error.set(
@@ -190,7 +392,7 @@ def vector_draw():
     )
 
     # Global space limit
-    limit= l0 + l1 + l2 + 1
+    limit= l[0] + l[1] + l[2] + 1
     ax.set_xlim([-limit, limit])
     ax.set_ylim([-limit, limit])
     ax.set_zlim([-limit, limit])